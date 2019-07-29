57c112d4546a7c75e006bfc10d3e71890f8b38e3
Brenda Lopez <yohanalopezrodas@gmail.com>
2019-07-29 11:22:14 -0700

    Abstract to extend SpaceObject

--- app/src/main/java/com/gamecodeschool/asteroidsfs/Particle.java
+++ app/src/main/java/com/gamecodeschool/asteroidsfs/Particle.java
@@ -8,19 +8,30 @@ public class Particle extends SpaceObject {
         super(pos, angle, velocityMagnitude, hitRadius);
     }
 
-//    Override
-//    public void update(float fps) {
-//        // Move the particle
-//        mPosition.x += mVelocity.x;
-//        mPosition.y += mVelocity.y;
-//    }
-//
-//    public void setPosition(PointF position) {
-//        mPosition.x = position.x;
-//        mPosition.y = position.y;
-//    }
-//
-//    public PointF getPosition() {
-//        return mPosition;
-//    }
+
+    // Default position update based on time.
+    public void update(long time, final Display screen) {
+        // UPDATING NEW POSITION VARIABLE.
+        position.x += velMagnitude * Math.cos(angle) * time;
+        position.y += velMagnitude * Math.sin(angle) * time;
+
+        if(position.x < 0) {
+            position.x = screen.width;
+        }
+        else if(position.y < 0) {
+            position.y = screen.height;
+        }
+        else if(position.x > screen.width) {
+            position.x = 0;
+        }
+        else if(position.y > screen.height) {
+            position.y = 0;
+        }
+    }
+
+
+    public void setPosition(PointF position) {
+        super.position.x = position.x;
+        super.position.y = position.y;
+    }
 }
