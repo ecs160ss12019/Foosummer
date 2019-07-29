def4927de12d0b85e5b8195b4ad53f70326e6221
Brenda Lopez <yohanalopezrodas@gmail.com>
2019-07-29 12:57:47 -0700

    Emit particle explosion for 0.5 seconds

--- app/src/main/java/com/gamecodeschool/asteroidsfs/ParticleSystem.java
+++ app/src/main/java/com/gamecodeschool/asteroidsfs/ParticleSystem.java
@@ -10,11 +10,12 @@ import java.util.Random;
 
 public class ParticleSystem {
 
-    float mDuration;
+    long mDuration;
 
     ArrayList<Particle> mParticles;
     Random random = new Random();
     boolean mIsRunning = false;
+    private final long EXPLOSION_INTERVAL = 500;
 
     //Maybe replace with a call to getObject() in ObjectFactory
     public void init(int numParticles, Display display){
@@ -27,27 +28,26 @@ public class ParticleSystem {
                                         (float)(random.nextInt(display.height)));
             float angle = (random.nextInt(360)) ;
             angle = angle * 3.14f / 180.f;
-            float speed = (random.nextInt(20)+1);
-            //float velocity = ((float)display.width) / 20 / 1000???
+            float speed = (random.nextInt(3)+1);
             mParticles.add(new Particle(position, angle, speed, 0));
         }
     }
 
 
-    public void update(long time, final Display screen, long fps) {
-        mDuration -= (1f/fps);
+    public void update(long time, final Display screen) {
+        mDuration += time;
 
         for(Particle p : mParticles)
             p.update(time, screen);
 
-        if (mDuration < 0)
+        if (mDuration > EXPLOSION_INTERVAL)
             mIsRunning = false;
     }
 
 
     public void emitParticles(PointF startPosition) {
         mIsRunning = true;
-        mDuration = 1f;
+        mDuration = 0;
 
         for(Particle p : mParticles){
             p.setPosition(startPosition);
@@ -57,11 +57,6 @@ public class ParticleSystem {
     public void draw(Canvas canvas, Paint paint) {
 
         for (Particle p : mParticles) {
-            //paint.setARGB(255, random.nextInt(256),
-            //random.nextInt(256),
-            //random.nextInt(256));
-
-            // Uncomment the next line to have plain white particles
             paint.setColor(Color.argb(255,255,255,255));
             canvas.drawRect(p.getPosition().x, p.getPosition().y,
                             p.getPosition().x+5, p.getPosition().y+5, paint);
