f1bae53b8115515536bacb466baeae8ff8948f80
Davy Chuon <dchuon@ucdavis.edu>
2019-08-01 16:13:43 -0700

    cleaned up PowerUps class and added documentation

--- app/src/main/java/com/gamecodeschool/asteroidsfs/PowerUps.java
+++ app/src/main/java/com/gamecodeschool/asteroidsfs/PowerUps.java
@@ -1,10 +1,6 @@
 package com.gamecodeschool.asteroidsfs;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.PointF;
-import android.graphics.RectF;
 import java.util.Random;
 
 enum PowerUp {
@@ -13,28 +9,21 @@ enum PowerUp {
 }
 
 public class PowerUps extends SpaceObject{
-//    private RectF mRect;
-//    float width, height, xVelocity, yVelocity;
-//    int hitsLeft;
-    private float size;
-    private PointF pos;
     private PowerUp type;
 
     public PowerUps(PointF pos, float size){
         super(pos, 0, 0, size);
-        this.size = size;
         this.type = getRandPowerUp();
-//        this.pos = pos;
-
     }
 
     @Override
     public void update(long fps, Display display){
         super.update(fps, display);
-
-        // do something to deal with player's position?
     }
 
+    // randomly selects from enum PowerUp
+    // to assign the PowerUp type being
+    // dropped (in constructor)
     private PowerUp getRandPowerUp(){
         Random rand = new Random();
         return PowerUp.values()[rand.nextInt(PowerUp.values().length)];
